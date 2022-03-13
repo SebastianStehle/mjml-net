dedc33a1ee364fe8d0c6df59a977c5ab361238c1
Sebastian <sebastian@squidex.io>
2022-03-13 22:13:03 +0100

    Rename sln.

--- Mjml.Net/MjmlRenderContext.cs
+++ Mjml.Net/MjmlRenderContext.cs
@@ -1,8 +1,6 @@
 ﻿using System.Xml;
 using Mjml.Net.Validators;
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-
 namespace Mjml.Net
 {
     public sealed partial class MjmlRenderContext : INode
