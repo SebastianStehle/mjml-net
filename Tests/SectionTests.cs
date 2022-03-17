7a34080c9c0b873330f871da281f8405ecab9514
Liam Riddell <web@liamriddell.co.uk>
2022-03-17 23:05:34 +0000

    mj-section

--- Tests/SectionTests.cs
+++ Tests/SectionTests.cs
@@ -7,11 +7,21 @@ namespace Tests
         [Fact]
         public void Should_render_section()
         {
-            var source = @"<mj-section background-color=""red""></mj-section>";
+            var source = @"<mj-section></mj-section>";
 
             var result = TestHelper.Render(source);
 
             AssertHelpers.HtmlAssert(TestHelper.GetContent("Section.html"), result);
+        }
+
+        [Fact]
+        public void Should_render_section_with_background_color()
+        {
+            var source = @"<mj-section background-color=""red""></mj-section>";
+
+            var result = TestHelper.Render(source);
+
+            AssertHelpers.HtmlAssert(TestHelper.GetContent("SectionWithBackgroundColor.html"), result);
         }
     }
 }
