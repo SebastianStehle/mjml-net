d7a721cc07880aeb6d1db2f651e348b9de097ee2
Liam Riddell <web@liamriddell.co.uk>
2022-03-17 22:16:54 +0000

    Added Css Class Suffix extension for strings

---
+++ Mjml.Net/Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Mjml.Net.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string? SuffixCssClasses(this string classAttributeValue, string? suffix)
+        {
+            if (string.IsNullOrEmpty(classAttributeValue))
+            {
+                return null;
+            }
+
+            if (string.IsNullOrEmpty(suffix))
+            {
+                return classAttributeValue;
+            }
+
+            var suffixedClasses = classAttributeValue
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(c => $"{c}-{suffix}");
+
+            return string.Join(" ", suffixedClasses);
+        }
+    }
+}
---
+++ Tests/CssClassesExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using Mjml.Net.Extensions;
+using Xunit;
+
+namespace Tests
+{
+    public class CssClassesExtensionsTests
+    {
+        [Fact]
+        public void Should_suffix_single_class()
+        {
+            var cssClass = "class1";
+            Assert.Equal("class1-outlook", cssClass.SuffixCssClasses("outlook"));
+        }
+
+        [Fact]
+        public void Should_suffix_multiple_classes()
+        {
+            var cssClass = "class1 class2";
+            Assert.Equal("class1-outlook class2-outlook", cssClass.SuffixCssClasses("outlook"));
+        }
+
+        [Fact]
+        public void Should_suffix_no_classes()
+        {
+            var cssClass = "         ";
+            Assert.Equal(string.Empty, cssClass.SuffixCssClasses("outlook"));
+        }
+    }
+}
