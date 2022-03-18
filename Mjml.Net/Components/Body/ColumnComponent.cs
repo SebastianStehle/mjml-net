c7c7ff48fc755f4263c4da89ce665827e404a2d0
Liam Riddell <web@liamriddell.co.uk>
2022-03-18 17:03:12 +0000

    mj-column

--- Mjml.Net/Components/Body/ColumnComponent.cs
+++ Mjml.Net/Components/Body/ColumnComponent.cs
@@ -1,11 +1,288 @@
-﻿namespace Mjml.Net.Components.Body
+﻿using Mjml.Net.Helpers;
+
+namespace Mjml.Net.Components.Body
 {
-    public partial class ColumnComponent : BodyComponentBase
+    public partial class ColumnComponent : Component, IProvidesWidth
     {
-        public override string ComponentName => "mj-col";
+        public override string ComponentName => "mj-column";
+
+        [Bind("background-color", BindType.Color)]
+        public string? BackgroundColor;
+
+        [Bind("border", BindType.String)]
+        public string? Border;
+
+        [Bind("border-bottom", BindType.String)]
+        public string? BorderBottom;
+
+        [Bind("border-left", BindType.String)]
+        public string? BorderLeft;
+
+        [Bind("border-radius", BindType.PixelsOrPercent)]
+        public string? BorderRadius;
+
+        [Bind("border-right", BindType.String)]
+        public string? BorderRight;
+
+        [Bind("border-top", BindType.String)]
+        public string? BorderTop;
+
+        [Bind("css-class")]
+        public string? Class;
+
+        [Bind("direction", BindType.Direction)]
+        public string Direction = "ltr";
+
+        [Bind("inner-background-color", BindType.Color)]
+        public string? InnerBackgroundColor;
+
+        [Bind("inner-border", BindType.String)]
+        public string? InnerBorder;
+
+        [Bind("inner-border-bottom", BindType.String)]
+        public string? InnerBorderBottom;
+
+        [Bind("inner-border-left", BindType.String)]
+        public string? InnerBorderLeft;
+
+        [Bind("inner-border-radius", BindType.PixelsOrPercent)]
+        public string? InnerBorderRadius;
+
+        [Bind("inner-border-right", BindType.String)]
+        public string? InnerBorderRight;
+
+        [Bind("inner-border-top", BindType.String)]
+        public string? InnerBorderTop;
+
+        [Bind("padding", BindType.PixelsOrPercent)]
+        public string? Padding;
+
+        [Bind("padding-bottom", BindType.PixelsOrPercent)]
+        public string? PaddingBottom;
+
+        [Bind("padding-left", BindType.PixelsOrPercent)]
+        public string? PaddingLeft;
+
+        [Bind("padding-right", BindType.PixelsOrPercent)]
+        public string? PaddingRight;
+
+        [Bind("padding-top", BindType.PixelsOrPercent)]
+        public string? PaddingTop;
+
+        [Bind("vertical-align", BindType.VerticalAlign)]
+        public string VerticalAlign = "top";
+
+        [Bind("width", BindType.PixelsOrPercent)]
+        public string? Width;
+
+        public ContainerWidth ContainerWidth;
+        public double CurrentWidth;
 
         public override void Render(IHtmlRenderer renderer, GlobalContext context)
         {
+            ContainerWidth = context.GetContainerWidth();
+
+            var (width, widthString, pixels) = GetParsedWidth();
+
+            var classesName = $"{GetColumnClass(width, widthString, context)} mj-outlook-group-fix";
+
+            if (!string.IsNullOrEmpty(Class))
+            {
+                classesName += $" {Class}";
+            }
+
+            renderer.ElementStart("div") // Style div
+                .Attr("class", classesName)
+                .Style("font-size", "0px")
+                .Style("text-align", "left")
+                .Style("direction", Direction)
+                .Style("display", "inline-block")
+                .Style("vertical-align", VerticalAlign)
+                .Style("width", "100%"); // Overriden by mj-column-per-*
+
+            if (HasGutter())
+            {
+                RenderGutter(renderer, context);
+            }
+            else
+            {
+                RenderColumn(renderer, context);
+            }
+
+            renderer.ElementEnd("div");
         }
+
+        private void RenderColumn(IHtmlRenderer renderer, GlobalContext context)
+        {
+            var tableElement = renderer.ElementStart("table") // Style table
+                .Attr("border", "0")
+                .Attr("cellpadding", "0")
+                .Attr("cellspacing", "0")
+                .Attr("role", "presentation")
+                .Attr("width", "100%");
+
+            if (HasGutter())
+            {
+                tableElement // Style `table` with gutter
+                    .Style("background-color", InnerBackgroundColor)
+                    .Style("border", InnerBorder)
+                    .Style("border-bottom", InnerBorderBottom)
+                    .Style("border-left", InnerBorderLeft)
+                    .Style("border-radius", InnerBorderRadius)
+                    .Style("border-right", InnerBorderRight)
+                    .Style("border-top", InnerBorderTop);
+            }
+            else
+            {
+                tableElement // Style `table`
+                    .Style("background-color", BackgroundColor)
+                    .Style("border", Border)
+                    .Style("border-bottom", BorderBottom)
+                    .Style("border-left", BorderLeft)
+                    .Style("border-radius", BorderRadius)
+                    .Style("border-right", BorderRight)
+                    .Style("border-top", BorderTop)
+                    .Style("vertical-align", VerticalAlign);
+            }
+
+            renderer.ElementStart("tbody");
+
+            foreach (var child in ChildNodes)
+            {
+                if (child.Raw)
+                {
+                    child.Render(renderer, context);
+                }
+                else
+                {
+                    renderer.ElementStart("tr");
+                    renderer.ElementStart("td")
+                        .Attr("align", child.Node.GetAttribute("align"))
+                        .Attr("vertical-align", child.Node.GetAttribute("vertical-align"))
+                        .Attr("class", child.Node.GetAttribute("css-class"))
+                        .Style("background", child.Node.GetAttribute("container-background-color"))
+                        .Style("font-size", "0px")
+                        .Style("padding", child.Node.GetAttribute("padding"))
+                        .Style("padding-top", child.Node.GetAttribute("padding-top"))
+                        .Style("padding-right", child.Node.GetAttribute("padding-right"))
+                        .Style("padding-bottom", child.Node.GetAttribute("padding-bottom"))
+                        .Style("padding-left", child.Node.GetAttribute("padding-left"))
+                        .Style("word-break", "break-word");
+
+                    child.Render(renderer, context);
+
+                    renderer.ElementEnd("td");
+                    renderer.ElementEnd("tr");
+                }
+            }
+
+            renderer.ElementEnd("tbody");
+            renderer.ElementEnd("table");
+        }
+
+        private void RenderGutter(IHtmlRenderer renderer, GlobalContext context)
+        {
+            renderer.ElementStart("table")
+                .Attr("border", "0")
+                .Attr("cellpadding", "0")
+                .Attr("cellspacing", "0")
+                .Attr("role", "presentation")
+                .Attr("width", "100%");
+
+            renderer.ElementStart("tbody");
+            renderer.ElementStart("tr");
+            renderer.ElementStart("td") // Style gutter
+                .Style("background-color", BackgroundColor)
+                .Style("border", Border)
+                .Style("border-bottom", BorderBottom)
+                .Style("border-left", BorderLeft)
+                .Style("border-radius", BorderRadius)
+                .Style("border-right", BorderRight)
+                .Style("border-top", BorderTop)
+                .Style("vertical-align", VerticalAlign)
+                .Style("padding", Padding)
+                .Style("padding-top", PaddingTop)
+                .Style("padding-right", PaddingRight)
+                .Style("padding-bottom", PaddingBottom)
+                .Style("padding-left", PaddingLeft);
+
+            RenderColumn(renderer, context);
+
+            renderer.ElementEnd("td");
+            renderer.ElementEnd("tr");
+            renderer.ElementEnd("tbody");
+            renderer.ElementEnd("table");
+        }
+
+        private static string GetColumnClass((double Value, Unit Unit) width, string originalWidth, GlobalContext context)
+        {
+            string className;
+
+            if (width.Unit == Unit.Percent)
+            {
+                className = $"mj-column-per-{width.Value}";
+            }
+            else
+            {
+                className = $"mj-column-px-{width.Value}";
+            }
+
+            context.SetGlobalData(originalWidth, MediaQuery.Width(className, originalWidth));
+
+            return className;
+        }
+
+        private ((double Value, Unit Unit), string, double) GetParsedWidth()
+        {
+            var widthValue = 0d;
+            var widthUnit = Unit.Pixels;
+            var widthString = string.Empty;
+            var pixels = 0d;
+
+            if (Width != null)
+            {
+                (widthValue, widthUnit) = UnitParser.Parse(Width);
+
+                // No need to interpolate it again.
+                widthString = Width;
+            }
+            else
+            {
+                widthValue = 100 / Math.Max(1, Parent?.ChildNodes.Count(x => !x.Raw) ?? 1);
+                widthUnit = Unit.Percent;
+                widthString = $"{widthValue}%";
+            }
+
+            if (widthUnit != Unit.Pixels)
+            {
+                pixels = ContainerWidth.Value * widthValue / 100;
+            }
+            else
+            {
+                pixels = widthValue;
+            }
+
+            return ((widthValue, widthUnit), widthString, pixels);
+        }
+
+        private bool HasGutter()
+        {
+            if (string.IsNullOrEmpty(Padding) ||
+                string.IsNullOrEmpty(PaddingBottom) ||
+                string.IsNullOrEmpty(PaddingLeft) ||
+                string.IsNullOrEmpty(PaddingRight) ||
+                string.IsNullOrEmpty(PaddingTop))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public double GetWidthAsPixel()
+        {
+            return CurrentWidth;
+        }
+
     }
 }
---
+++ Tests/ColumnTests.cs
@@ -0,0 +1,72 @@
+﻿using Xunit;
+
+namespace Tests
+{
+    public class ColumnTests
+    {
+        [Fact]
+        public void Should_render_column_one()
+        {
+            var source = @"<mj-column background-color=""red""></mj-column>";
+
+            var result = TestHelper.Render(source);
+
+            AssertHelpers.HtmlAssert(TestHelper.GetContent("ColumnOne.html"), result);
+        }
+
+        [Fact]
+        public void Should_render_column_two()
+        {
+            var source = @"
+<mjml-test head=""false"">
+<mj-column background-color=""red""></mj-column>
+<mj-column background-color=""green""></mj-column>
+</mjml-test>";
+
+            var result = TestHelper.Render(source);
+
+            AssertHelpers.HtmlAssert(TestHelper.GetContent("ColumnTwo.html"), result);
+        }
+
+        [Fact]
+        public void Should_render_column_three()
+        {
+            var source = @"
+<mjml-test head=""false"">
+<mj-column background-color=""red""></mj-column>
+<mj-column background-color=""green""></mj-column>
+<mj-column background-color=""blue""></mj-column>
+</mjml-test>";
+
+            var result = TestHelper.Render(source);
+
+            AssertHelpers.HtmlAssert(TestHelper.GetContent("ColumnThree.html"), result);
+        }
+
+        [Fact]
+        public void Should_render_column_four()
+        {
+            var source = @"
+<mjml-test head=""false"">
+<mj-column background-color=""red""></mj-column>
+<mj-column background-color=""green""></mj-column>
+<mj-column background-color=""blue""></mj-column>
+<mj-column background-color=""yellow""></mj-column>
+</mjml-test>";
+
+            var result = TestHelper.Render(source);
+
+            AssertHelpers.HtmlAssert(TestHelper.GetContent("ColumnFour.html"), result);
+        }
+
+        [Fact]
+        public void Should_render_column_one_with_padding()
+        {
+            var source = @"<mj-column background-color=""red"" padding=""20px 52px""></mj-column>";
+
+            var result = TestHelper.Render(source);
+
+            AssertHelpers.HtmlAssert(TestHelper.GetContent("ColumnOneWithPadding.html"), result);
+        }
+    }
+}
