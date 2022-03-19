db66832a94584bf1a4ebc04236f46e3c567fd808
Sebastian Stehle <sebastian@squidex.io>
2022-03-19 12:37:58 +0100

    Adds background color to body. (#61)

---
+++ Mjml.Net/Helpers/Background.cs
@@ -0,0 +1,6 @@
+﻿namespace Mjml.Net.Helpers
+{
+#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
+    public sealed record Background(string Color);
+#pragma warning restore SA1313 // Parameter names should begin with lower-case letter
+}
