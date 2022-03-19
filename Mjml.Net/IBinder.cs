ff8039663568d98365ac469ade2da8d29a590a9e
Sebastian <sebastian@squidex.io>
2022-03-19 17:42:09 +0100

    Add missing class.

---
+++ Mjml.Net/IBinder.cs
@@ -0,0 +1,20 @@
+﻿namespace Mjml.Net
+{
+    public interface IBinder
+    {
+        /// <summary>
+        /// Get the attribute of the node with the given name.
+        /// </summary>
+        /// <param name="name">The name of the attribute.</param>
+        /// <returns>
+        /// The attribute of the node or null if not found.
+        /// </returns>
+        string? GetAttribute(string name);
+
+        /// <summary>
+        /// Get the text content of the node.
+        /// </summary>
+        /// <returns>The content of the node or null if not found.</returns>
+        string? GetText();
+    }
+}
