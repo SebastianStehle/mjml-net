caf61164a88e4098c9fcbebce3390928b7979513
Liam Riddell <3812154+LiamRiddell@users.noreply.github.com>
2022-03-22 21:56:33 +0000

    Added more templates to benchmarking (#74)
    * Added more templates to benchmarking
    
    * Updated Benchmark
    
    * Added dynamic template resolution.
    
    * Copied templates from TestRunner.
    
    * Merging TestRunner and Benchmarking
    
    * Renamed Mjml.Net.Benchmarking to Mjml.Net.Benchmark
    
    * Removed TestRunner
    
    * Removed TestRunner
    
    * Added Dynamic Discovery to TestRunner mode.

---
+++ Mjml.Net.Benchmark/TemplateBenchmarks.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Mjml.Net.Benchmarking
+{
+    public class TemplateBenchmarks
+    {
+        private readonly MjmlRenderer MjmlRenderer;
+     
+        [ParamsSource(nameof(MjmlTemplates))]
+        public string MjmlTemplateFilePath { get; set; }
+
+        public IEnumerable<string> MjmlTemplates => Directory.GetFiles("./Templates/", "*.mjml");
+
+        public string MjmlTemplate { get; set; }
+
+        public TemplateBenchmarks()
+        {
+            MjmlRenderer = new();
+        }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            MjmlTemplate = File.ReadAllText(MjmlTemplateFilePath);
+        }
+
+        [Benchmark()]
+        public string Render_Template_Beautify()
+        {
+            return MjmlRenderer.Render(MjmlTemplate, new MjmlOptions
+            {
+                Beautify = true
+            }).Html;
+        }
+
+        [Benchmark]
+        public string Render_Template_Minify()
+        {
+            return MjmlRenderer.Render(MjmlTemplate, new MjmlOptions
+            {
+                Beautify = true
+            }).Html;
+        }
+    }
+}
