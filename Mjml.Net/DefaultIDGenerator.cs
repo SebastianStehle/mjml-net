b93e0d6d7dd51697547cdc782c1bbbfffa3fbfbc
Sebastian <sebastian@squidex.io>
2022-03-23 08:45:44 +0100

    Improve tests2

---
+++ Mjml.Net/DefaultIDGenerator.cs
@@ -0,0 +1,27 @@
+﻿namespace Mjml.Net
+{
+    /// <summary>
+    /// The default ID generator.
+    /// </summary>
+    public sealed class DefaultIDGenerator : IIdGenerator
+    {
+        private int counter;
+
+        /// <summary>
+        /// The only instance of the <see cref="DefaultIDGenerator"/>.
+        /// </summary>
+        public static readonly IIdGenerator Instance = new DefaultIDGenerator();
+
+        private DefaultIDGenerator()
+        {
+        }
+
+        /// <inheritdoc />
+        public string Next()
+        {
+            Interlocked.Increment(ref counter);
+
+            return $"{counter}";
+        }
+    }
+}
---
+++ Tests/Internal/StaticIdGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mjml.Net;
+
+namespace Tests.Internal
+{
+    internal sealed class StaticIdGenerator : IIdGenerator
+    {
+        private readonly string[] values;
+        private int position = -1;
+
+        public StaticIdGenerator(params string[] values)
+        {
+            this.values = values;
+        }
+
+        public string Next()
+        {
+            position++;
+
+            if (position == values.Length)
+            {
+                position = 0;
+            }
+
+            return values[position];
+        }
+    }
+}
