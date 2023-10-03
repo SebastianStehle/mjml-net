02e34ee3e99ffbe652d87720131afd4c3c4c7282
Artem Balianytsia <artem.balianytsia@dvigunity.com>
2023-10-03 15:42:10 +0200

    Added xml comments

--- Mjml.Net/FilesystemFileLoader.cs
+++ Mjml.Net/FilesystemFileLoader.cs
@@ -12,6 +12,12 @@ public class FilesystemFileLoader : IFileLoader
     private readonly string baseFolder;
     private readonly Encoding encoding;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FilesystemFileLoader"/> class.
+    /// </summary>
+    /// <param name="baseFolder">A base folder for relative includes. By default it should be the directory of the rendering file.</param>
+    /// <param name="encoding">The encoding to use when reading files.</param>
+    /// <param name="enableCaching">Indicates if loader should cache the content of loaded files for future usage.</param>
     public FilesystemFileLoader(string baseFolder, Encoding? encoding, bool enableCaching = true)
     {
         this.baseFolder = baseFolder;
@@ -19,7 +25,14 @@ public class FilesystemFileLoader : IFileLoader
         this.encoding = encoding ?? Encoding.UTF8;
     }
 
+    /// <summary>
+    /// Indicates if loader should cache the content of loaded files for future usage.
+    /// </summary>
     public bool EnableCaching { get; set; }
+
+    /// <summary>
+    /// A concurrent dictionary of already loaded files. You can modify this dictionary externally to preload files.
+    /// </summary>
     public ConcurrentDictionary<string, string> CachedFiles { get; } = new (new FilePathEqualityComparer());
 
     public virtual string LoadText(IncludeComponent.FileContext context)
